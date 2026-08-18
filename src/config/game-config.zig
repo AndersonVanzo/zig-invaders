6dca59aa7e4a4089690a032d0568de798f98a00b
AndersonVanzo <anderson.vanzo01@gmail.com>
2026-08-18 01:44:39 -0300

    feat: initializing game config

--- src/config/game-config.zig
+++ src/config/game-config.zig
@@ -6,28 +6,35 @@ pub const GameConfig = struct {
     // player
     player_height: i32,
     player_width: i32,
-    player_speed: i32,
+    player_speed: u8,
     player_start_x: i32,
     player_start_y: i32,
 
     // player bullet
     player_bullet_height: i32,
     player_bullet_width: i32,
-    player_bullet_speed: i32,
+    player_bullet_speed: u8,
+    max_player_bullets: u8,
 
     // invader
     invader_height: i32,
     invader_width: i32,
-    invader_speed: i32,
+    invader_speed: u8,
     invader_start_x: i32,
     invader_start_y: i32,
     invader_spacing_x: i32,
     invader_spacing_y: i32,
+    invader_shoot_change: u8,
+    invader_move_delay: u8,
+    invader_drop_distance: u8,
+    invader_rows: u8,
+    invader_columns: u8,
 
     // invader bullet
     invader_bullet_height: i32,
     invader_bullet_width: i32,
-    invader_bullet_speed: i32,
+    invader_bullet_speed: u8,
+    max_invader_bullets: u8,
 
     // shield
     shield_height: i32,
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,54 @@
 const rl = @import("raylib");
+const GameConfig = @import("zig_invaders").GameConfig;
 
 pub fn main() void {
-    rl.initWindow(500, 500, "Zig Invaders");
+    const config = GameConfig{
+        // window
+        .screen_height = 600,
+        .screen_width = 800,
+
+        // player
+        .player_height = 30,
+        .player_width = 50,
+        .player_speed = 5,
+        .player_start_x = 265,
+        .player_start_y = 540,
+
+        // player bullet
+        .player_bullet_height = 10,
+        .player_bullet_width = 4,
+        .player_bullet_speed = 10,
+        .max_player_bullets = 10,
+
+        // invader
+        .invader_height = 30,
+        .invader_width = 40,
+        .invader_speed = 5,
+        .invader_start_x = 100,
+        .invader_start_y = 50,
+        .invader_spacing_x = 60,
+        .invader_spacing_y = 40,
+        .invader_shoot_change = 5,
+        .invader_move_delay = 30,
+        .invader_drop_distance = 20,
+        .invader_rows = 5,
+        .invader_columns = 11,
+
+        // invader bullet
+        .invader_bullet_height = 10,
+        .invader_bullet_width = 4,
+        .invader_bullet_speed = 10,
+        .max_invader_bullets = 20,
+
+        // shield
+        .shield_height = 60,
+        .shield_width = 80,
+        .shield_start_x = 150,
+        .shield_start_y = 450,
+        .shield_spacing_x = 150,
+    };
+
+    rl.initWindow(config.screen_width, config.screen_height, "Zig Invaders");
     defer rl.closeWindow();
 
     while (!rl.windowShouldClose()) {
